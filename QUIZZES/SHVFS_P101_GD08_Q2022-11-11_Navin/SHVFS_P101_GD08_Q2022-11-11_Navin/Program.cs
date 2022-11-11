0951fb60b0c0588f6af7e9608292f37f308bd91c
Navinnnnnnnnnn <2415578234@qq.com>
2022-11-11 20:10:28 +0800

    324234234

---
+++ QUIZZES/SHVFS_P101_GD08_Q2022-11-11_Navin/SHVFS_P101_GD08_Q2022-11-11_Navin/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace P101_Quiz2
+{
+    internal class Program
+    {
+
+
+        struct Position
+        {
+            public int X;
+            public int Y;
+            public int Z;
+
+           
+
+            public Position(int x, int y, int z)
+            { 
+                X = x;
+                Y = y;
+                Z = z;
+            
+            }
+
+
+        }
+
+        public class GameObject
+        {
+            string Name;
+
+         
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+            GameObject obj = new GameObject();
+            Console.WriteLine("Hello! GoodBye!");
+            Console.ReadLine();
+        }
+
+
+    }
+   
+
+
+
+}
