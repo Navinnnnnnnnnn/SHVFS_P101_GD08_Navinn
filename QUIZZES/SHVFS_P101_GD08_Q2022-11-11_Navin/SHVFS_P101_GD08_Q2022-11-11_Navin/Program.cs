7e06207f96b444f7920703ceed92aa57d80dd4ab
Navinnnnnnnnnn <2415578234@qq.com>
2022-11-11 22:55:22 +0800

    435345345

--- QUIZZES/SHVFS_P101_GD08_Q2022-11-11_Navin/SHVFS_P101_GD08_Q2022-11-11_Navin/Program.cs
+++ QUIZZES/SHVFS_P101_GD08_Q2022-11-11_Navin/SHVFS_P101_GD08_Q2022-11-11_Navin/Program.cs
@@ -2,8 +2,37 @@
 {
     internal class Program
     {
+        public static void Main(string[] args)
+        {
+            int goCount = 5;
+            var goArray = new GameObject[5];
+
+            var go1 = new GameObject("1", new Position());
+            var go2 = new GameObject("2", new Position());
+            var go3 = new GameObject("3", new Position());
+            var go4 = new GameObject("4", new Position());
+            var go5 = new GameObject("5", new Position());
+
+            goArray[0] = go1;
+            goArray[1] = go2;
+            goArray[2] = go3;
+            goArray[3] = go4;
+            goArray[4] = go5;
+
+            for (int i = 0; i < goArray.Length; i++)
+            {
+                for (int j = 0; j < goArray.Length; j++)
+                {
+                    if (i == j) continue;
+
+                    if (goArray[i].CheckMatchingPsition(goArray[j]))
+                }
+            }
+                
+            
 
 
+        }
         struct Position
         {
             public int X;
@@ -25,20 +54,15 @@
 
         public class GameObject
         {
-            string Name;
+            public string Name;
+            public Position Position
+
 
          
 
         }
 
-        public static void Main(string[] args)
-        {
-
-            GameObject obj = new GameObject();
-            Console.WriteLine("Hello! GoodBye!");
-            Console.ReadLine();
-        }
-
+        
 
     }
    
---
+++ homework/SHVFS_P101_GD08_NAVIN/SHVFS_P101_GD08_NAVIN/Program.cs
@@ -0,0 +1,103 @@
+﻿
+
+    namespace SHVFS_P101_GD08_Navin
+    {
+        public struct position
+        {
+            public float X;
+            public float Y;
+            public float Z;
+            public position(float x, float y, float z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+
+            }
+
+
+
+        }
+
+        public class posi
+        {
+            public position pos;
+            public string name;
+
+            public posi(string name, position pos)
+            {
+                this.pos = pos;
+                this.name = name;
+            }
+
+
+            public bool CheckPosition(posi go)
+            {
+                if (go.pos.X == pos.X && go.pos.Y == pos.Y && go.pos.Z == pos.Z)
+                {
+                    return true;
+                }
+                return false;
+
+
+
+            }
+
+
+
+
+        }
+
+
+        public class Program
+        {
+
+
+
+            public static void Main(string[] args)
+            {
+
+
+
+                posi[] go = new posi[5];
+                go[0] = new posi("0", new position(0, 20, 60));
+                go[1] = new posi("1", new position(10, 20, 60));
+                go[2] = new posi("2", new position(20, 20, 60));
+                go[3] = new posi("3", new position(0, 20, 60));
+                go[4] = new posi("4", new position(100, 20, 60));
+                for (int i = 0; i < go.Length - 1; i++)
+                {
+
+                    for (int j = i + 1; j < go.Length; j++)
+                    {
+
+
+                        if (go[i].CheckPosition(go[j]))
+
+                        {
+
+                            Console.WriteLine($"{go[i].name} and {go[j].name} have the same position");
+                        }
+
+
+                    }
+
+
+                }
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
