acb38969c4ea497eea048ee3edbcd348042c375c
Navinnnnnnnnnn <2415578234@qq.com>
2022-11-11 19:08:34 +0800

    546456

---
+++ QUIZZES/SHVFS_P101_GD08_Q2022-11-10_Navin/SHVFS_P101_GD08_Q2022-11-10_Navin/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace guessing
+{
+
+    class guess
+    {
+
+        public static void Main(string[] args)
+        {
+            string name, reply;
+            int i = 0;
+            
+
+            string[] words = { "apple", "banana", "cat", "dog", "eye", "food" };
+            Random rand = new Random();
+            int word = rand.Next(words.Length);
+            Console.WriteLine("Hi! Welcome to the word guessing game!\n" +
+                                  "Now,Enter your name pleeeeeeeeeeease!");
+           
+            name = Console.ReadLine();
+           
+            Console.WriteLine($"Hi!{name},nice to meet you!Now,Let's start the game!");
+         
+            Console.WriteLine("The words are apple, banana, cat, dog, eye, food.");
+
+            reply = Console.ReadLine();
+     
+                while (reply != words[word])
+                {
+                    Console.WriteLine("Wrong");
+                    Console.WriteLine("Try again");
+                    reply = Console.ReadLine();
+                    Console.Clear();
+                    i = i + 1;
+                   
+                    
+                }
+            Console.WriteLine("Nice guessing!You're awesome!!!!!!!!!\n" +
+
+
+                $"   You have tried  {i}  times! ");   
+               
+             
+                
+
+            
+
+
+
+
+
+        }
+    }
+}
+
+   
